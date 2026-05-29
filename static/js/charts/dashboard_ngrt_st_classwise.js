f50ae9fc434fa3d881083198bbdf7428f9bff460
iam-awingsuma <acsumalinog1@up.edu.ph>
2026-05-30 02:03:31 +0400

    feat: enhance NGRT stanine attainment chart with improved hover label font size and legend styling

--- static/js/charts/dashboard_ngrt_st_classwise.js
+++ static/js/charts/dashboard_ngrt_st_classwise.js
@@ -31,6 +31,7 @@ async function renderNGRTClasswiseStanineChart() {
       name: "Below Average",
       type: "bar",
       marker: { color: "#FF5A5A" },
+      hoverlabel: {font: { size: 11 }},
       hovertemplate:
         "<b>Year %{x}</b><br>" +
         "Band: Below Average<br>" +
@@ -45,6 +46,7 @@ async function renderNGRTClasswiseStanineChart() {
       name: "Average",
       type: "bar",
       marker: { color: "#FCB53B" },
+      hoverlabel: {font: { size: 11 }},
       hovertemplate:
         "<b>Year %{x}</b><br>" +
         "Band: Average<br>" +
@@ -59,6 +61,7 @@ async function renderNGRTClasswiseStanineChart() {
       name: "Above Average",
       type: "bar",
       marker: { color: "#A7E399" },
+      hoverlabel: {font: { size: 11 }},
       hovertemplate:
         "<b>Year %{x}</b><br>" +
         "Band: Above Average<br>" +
